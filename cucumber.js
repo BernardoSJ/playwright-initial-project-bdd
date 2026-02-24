1cb119e6a2203fdd99f26e0855576404121fc0fe
BernardoSJ <salinasjaquezb@gmail.com>
2026-02-24 10:41:10 -0600

    Add upload cucumber result and reports and change parallel to 1

--- cucumber.js
+++ cucumber.js
@@ -2,10 +2,10 @@ module.exports = {
   default: {
     requireModule: ['ts-node/register', 'tsconfig-paths/register'],
     require: ['src/support/world.ts', 'src/support/hooks.ts', 'src/steps/*.ts'],
-    format: ['progress', 'html:reports/cucumber/index.html', 'json:reports/cucumber/report.json', 'allure-cucumberjs/reporter'],
+    format: ['progress', 'summary', 'html:reports/cucumber/index.html', 'json:reports/cucumber/report.json', 'allure-cucumberjs/reporter'],
     formatOptions: { resultsDir: "allure-results"},
     publishQuiet: true,
-    parallel: 2,
+    parallel: 1,
     paths: ['features/**/*.feature'],
     tags: 'not @wip',
   },
