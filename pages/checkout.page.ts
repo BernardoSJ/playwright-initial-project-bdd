bc555f370bad6fecc556142c8ce87362a33bedbc
BernardoSJ <salinasjaquezb@gmail.com>
2026-02-24 11:08:36 -0600

    Add unfailed tests

--- pages/checkout.page.ts
+++ pages/checkout.page.ts
@@ -30,8 +30,6 @@ export class CheckoutPage {
       this.continueButton.click(),
     ]);
     await expect(this.finishButton).toBeVisible();
-    let test = 30;
-    expect(test).toBe(27);
   }
 
   async clickFinishButton() {
